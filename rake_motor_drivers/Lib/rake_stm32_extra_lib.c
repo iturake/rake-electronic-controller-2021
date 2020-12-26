79237777627641dc1611d603ad6a54601bc35f7f
Bedirhan Önder <63157533+onderbedirhan@users.noreply.github.com>
2020-12-26 13:40:21 +0300

    Resolves rake_extra_lib

--- rake_motor_drivers/Lib/rake_stm32_extra_lib.c
+++ rake_motor_drivers/Lib/rake_stm32_extra_lib.c
@@ -1,6 +1,40 @@
 
 /* Private includes ----------------------------------------------------------*/
+
 #include "rake_stm32_extra_lib.h"
 
+/* Private structures -----------------------------------------------*/
+
+struct flagStruct flag = {{0,0,0,0,0,0,0,0},{0,0}};
+
+/* Private variables -----------------------------------------------*/
+
+uint32_t motorBackward_Pin = TIM_CHANNEL_1;
+uint32_t motorForward_Pin = TIM_CHANNEL_2;
+uint32_t encoderA_Pin = TIM_CHANNEL_1;
+uint32_t encoderB_Pin = TIM_CHANNEL_2;
+
+
 /* Private functions -----------------------------------------------*/
+
+float RAKE_Convert(uint8_t convertMode_u8, float convertingValue_f32) {
+	
+	uint32_t convert_result ;
+	
+	switch(convertMode_u8){
+		case VOLTAGE_TO_PWM:
+			convert_result = PWM_ZERO + (convertingValue_f32 / VOLTAGE_MAX) * (PWM_MAX - PWM_ZERO);
+			break;
+		case PWM_TO_VOLTAGE:
+			convert_result = (convertingValue_f32 / PWM_MAX) * VOLTAGE_MAX;
+			break;
+		case RPM_TO_PWM:
+			convert_result = (convertingValue_f32 / RPM_MAX) * PWM_MAX;
+			break;
+		case PWM_TO_RPM:
+			convert_result = (convertingValue_f32 / PWM_MAX) * RPM_MAX;
+			break;
+	}
+	return convert_result;
+}
 
--- rake_motor_drivers/Lib/rake_stm32_extra_lib.h
+++ rake_motor_drivers/Lib/rake_stm32_extra_lib.h
@@ -7,10 +7,27 @@ extern "C" {
 
 
 /* Private includes ----------------------------------------------------------*/
-
+#include "stm32f1xx_hal.h"
 
 /* Private defines ----------------------------------------------------------*/
 
+#define GEAR_RATIO 1
+#define ENCODER_PULS 720
+
+#define VOLTAGE_MAX 12.0
+#define RPM_MAX 120.0
+
+#define PWM_MAX 999
+#define PWM_ZERO 499
+#define PWM_MIN 0
+
+#define TX_TIME 99
+#define PID_TIME 9
+#define LED_TIME 49
+#define VELOCITY_TIME 999
+
+#define SECOND 1000
+#define MINUTE 60
 
 /* Private pin variables ----------------------------------------------------------*/
 
@@ -18,17 +35,37 @@ extern "C" {
 
 /* Private enums ----------------------------------------------------------*/
 
+enum {
+	VOLTAGE_TO_PWM,
+	PWM_TO_VOLTAGE,
+	RPM_TO_PWM,
+	PWM_TO_RPM
+};
 
 
 /* Private structures ----------------------------------------------------------*/
 
+struct flagStruct {
+	struct {
+		_Bool motorForward_bit;
+		_Bool motorBackward_bit;
+		_Bool UART_bit;
+		_Bool CANBUS_bit;
+		_Bool adminMode_bit;
+		_Bool testMode_bit;
+		_Bool normalMode_bit;
+		_Bool ERROR_bit;
+	} LED;
+	struct {
+		uint8_t rxIndex_bool;
+		_Bool rxComplete_bool;
+	} UART;
+};
 
 
+/* Private function prototypes -----------------------------------------------*/
 
-
-/* Private functions -----------------------------------------------*/
-
-
+float RAKE_Convert(uint8_t convertMode_u8, float convertingValue_f32);
 
 
 
